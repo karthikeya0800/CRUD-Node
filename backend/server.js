c58f117f1e337b36c477b4995a806fd989945acf
karthikeya0800 <karthikeya0800@gmail.com>
2024-09-13 12:32:10 +0530

    added server port in env

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,6 @@ connectDB();
 app.use("/api", itemRoutes);
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
